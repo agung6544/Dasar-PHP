9cead7a6cd487525e31eebf8e1f0c4a80b1a1bb7
agung6544 <agung.nainggolan8008@gmail.com>
2022-12-15 16:03:48 +0700

    Dasar saja

---
+++ latihan.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ini latihan php dasar</title>
+</head>
+<body>
+    <h1>hello world</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum dicta tempore est incidunt qui veniam repudiandae quam placeat in exercitationem? Placeat esse eaque libero fugit cum sit? Deserunt, magni voluptates.</p>
+    <br>
+    <!-- ini adalah kodingan php -->
+    <?php
+echo "<h2>PHP is Fun!</h2>";
+echo "Hello world!<br>";
+echo "I'm about to learn PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+?>
+</body>
+</html>
